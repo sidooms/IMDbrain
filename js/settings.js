6329c67629abbd25c9ff9a62104424c7950ee3e4
Simon Dooms <simon.dooms@intec.ugent.be>
2014-07-15 15:04:15 +0200

    Updated URL for API calls, included imdbrain.com website

--- js/settings.js
+++ js/settings.js
@@ -1,5 +1,5 @@
-brain_api_server ="some.host.com"
+brain_api_server ="api.imdbrain.com"
 brain_api_path = "http://" + brain_api_server + "/IMDbrain/api.php"
 brain_movie_poster_path = "http://" + brain_api_server + "/IMDbrain/getmovieposter.php"
-brain_img_not_found_url = "http://" + brain_api_server + "/IMDbrain/img/movieposters/not_found.gif"
+brain_img_not_found_url = "http://" + brain_api_server + "/moviebrain/img/movieposters/not_found.gif"
     
